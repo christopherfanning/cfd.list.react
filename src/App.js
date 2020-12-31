351ce5412b6d4dcdf105777c9df3c4fcbbc0ff9c
christopherfanning <christopherleefanning@gmail.com>
2020-12-30 19:56:11 -0600

    Fix images displaying, moved into public directory

--- src/App.js
+++ src/App.js
@@ -3,29 +3,11 @@ import Person from "./Person";
 import house from "./Data";
 import "./Person";
 
-// function importAll(r) {
-//   return r.keys().map(r);
-// }
-
-// const images = importAll(
-//   require.context("./assets/img/houses", false, /\.(png|jpe?g|svg)$/)
-// );
-
-function importAll(r) {
-  let images = {};
-  r.keys().map((item, index) => {
-    images[item.replace("./", "")] = r(item);
-  });
-  return images;
-}
-
-const images = importAll(require.context("./", false, /\.(png|jpe?g|svg)$/));
 function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
       <h1>Houses</h1>
-      <img src={"assets/img/houses/E1.jpg"} />
       {house.map((value, index) => {
         return <Person index={index}>{value}</Person>;
       })}
--- src/Person.js
+++ src/Person.js
@@ -1,25 +1,11 @@
 import React from "react";
 import house from "./Data";
 
-function importAll(r) {
-  let images = {};
-  r.keys().map((item, index) => {
-    images[item.replace("./", "")] = r(item);
-  });
-  return images;
-}
-
-const images = importAll(
-  require.context("./assets/img/houses", false, /\.(png|jpe?g|svg)$/)
-);
-
 const person = (props) => {
   return (
     <div className="house">
-      <img src={images[0]} alt="" />
-      <img src={images[house[props.index].profilePic]} />
       <div className="houseImg">
-        <img src={images[props.index]} />
+        <img src={[house[props.index].profilePic]} alt="" />
       </div>
       <h3>I'm {house[props.index].name}</h3>
       <p>{house[props.index].address}</p>
